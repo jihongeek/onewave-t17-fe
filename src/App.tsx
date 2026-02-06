ec1cfc952e3c9a5845712e6d75a7dd1dcfd4cae9
kss2002 <kss2002@users.noreply.github.com>
2026-02-06 19:35:16 +0900

    fix: tailwind project setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import './App.css';
-
 function App() {
-  return '';
+  return (
+    <>
+      <p className="text-3xl text-blue-500 font-bold underline">Hello world!</p>
+    </>
+  );
 }
 
 export default App;
